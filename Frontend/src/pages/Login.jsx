0507327b249b91b80ffbb1c2c7764c43fbc6eff4
Brendan <brplahey@gmail.com>
2024-03-28 19:13:21 -0500

    login page started

---
+++ Frontend/src/pages/Login.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import styled from "styled-components";
+import Form from "../components/Form";
+
+const Login = () => {
+  return (
+    <LoginStyled>
+      <Form />
+    </LoginStyled>
+  );
+};
+
+const LoginStyled = styled.div``;
+export default Login;
