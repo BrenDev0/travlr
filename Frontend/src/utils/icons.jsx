3f196244db2aa9d7abacc6a45faa22f104e639e5
Brendan Lahey <lahey1991@gmail.com>
2024-05-22 21:13:41 -0500

    preparing icons

--- Frontend/src/utils/icons.jsx
+++ Frontend/src/utils/icons.jsx
@@ -1,1 +1,7 @@
-export const foodIcon = "icons"
+export const cateringIcon = <i className="fa-solid fa-utensils"></i>
+export const museumIcon = <i className="fa-solid fa-building-columns"></i>
+export const accomidationIcon = <i className="fa-solid fa-bed"></i>
+export const commercialIcon = <i className="fa-solid fa-store"></i>
+export const entertainmentIcon = <i className="fa-solid fa-masks-theater"></i>
+export const naturalIcon = <i className="fa-solid fa-tree"></i>
+export const tourismIcon = <i className="fa-solid fa-van-shuttle"></i>
